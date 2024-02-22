cf692856e8359cca1cb7af421564c91bdee676bb
carlosebl <carlosebl@gmail.com>
2024-02-22 15:05:38 -0300

    construida estrutura do projeto

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/carlosebl.png" />
+    <Titulo>Carlos Leite</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,15 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre mim</Titulo>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, veniam
+      porro quo vitae cum doloremque sit dolorum maiores quae distinctio eum
+      assumenda similique autem adipisci aspernatur repellendus rem animi
+      minima?
+    </p>
+  </section>
+)
+
+export default Sobre
