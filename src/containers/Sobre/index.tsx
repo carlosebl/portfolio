a7225b3c135c2a52d9a42f0d992d852a774109cc
carlosebl <carlosebl@gmail.com>
2024-02-23 13:32:16 -0300

    finalizada seção sobre mim

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,5 +1,6 @@
 import Titulo from '../../components/Titulo'
 import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
 
 const Sobre = () => (
   <section>
@@ -10,6 +11,10 @@ const Sobre = () => (
       assumenda similique autem adipisci aspernatur repellendus rem animi
       minima?
     </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=carlosebl&show_icons=true&theme=dracula" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=carlosebl&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
   </section>
 )
 
---
+++ src/containers/Sobre/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const GithubSecao = styled.div`
+  margin-top: 32px;
+  margin-bottom: 64px;
+  max-width: 744px;
+
+  img {
+    height: 160px;
+  }
+`
