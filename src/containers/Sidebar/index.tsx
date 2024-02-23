e37a4332bf2a51d94e872461739dec1ad9a034bc
carlosebl <carlosebl@gmail.com>
2024-02-23 10:54:34 -0300

    criados componentes para textos

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -3,7 +3,7 @@ import Titulo from '../../components/Titulo'
 const Sidebar = () => (
   <aside>
     <img src="https://github.com/carlosebl.png" />
-    <Titulo>Carlos Leite</Titulo>
+    <Titulo fontSize={20}>Carlos Leite</Titulo>
   </aside>
 )
 
--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,14 +1,15 @@
 import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
 
 const Sobre = () => (
   <section>
     <Titulo>Sobre mim</Titulo>
-    <p>
+    <Paragrafo tipo="principal">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, veniam
       porro quo vitae cum doloremque sit dolorum maiores quae distinctio eum
       assumenda similique autem adipisci aspernatur repellendus rem animi
       minima?
-    </p>
+    </Paragrafo>
   </section>
 )
 
