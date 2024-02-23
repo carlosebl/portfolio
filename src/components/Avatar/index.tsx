318741bd2b8c8fa0339565f83bdaecaec0a5413d
carlosebl <carlosebl@gmail.com>
2024-02-23 15:22:33 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="http://github.com/carlosebl.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/carlosebl.png" />
-    <Titulo fontSize={20}>Carlos Leite</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Carlos Leite</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        carlosebl
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-End
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
